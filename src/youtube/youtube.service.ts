3b7e882adeb62a0a1676cb85fc4c2f98dd7ba78a
Diones <pereiradiones987@gmail.com>
2025-03-20 19:34:22 -0300

    criando service

--- src/youtube/youtube.service.ts
+++ src/youtube/youtube.service.ts
@@ -1,4 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import axios from 'axios';
 
 @Injectable()
-export class YoutubeService {}
+export class YoutubeService {
+  private readonly apiKey: string | undefined;
+  private readonly apiUrl: string = 'https://www.googleapis.com/youtube/v3/search';
+
+  constructor(private configService: ConfigService) {
+    this.apiKey = this.configService.get<string>('YOUTUBE_API_KEY');
+  }
+
+  async searchVideos(query: string, maxResults: number = 10): Promise<any> {
+    const params = {
+      part: 'snippet',
+      q: query,
+      maxResults: maxResults,
+      key: this.apiKey,
+    };
+
+    try {
+      const response = await axios.get(this.apiUrl, { params });
+      return response.data;
+    } catch (error) {
+      throw new Error('Erro ao buscar vídeos no Youtube');
+    }
+  }
+}
