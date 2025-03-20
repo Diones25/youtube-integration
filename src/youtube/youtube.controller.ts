5184d3ee3be20be7482496366bd6bae970237bc5
Diones <pereiradiones987@gmail.com>
2025-03-20 19:37:53 -0300

    criando controller

--- src/youtube/youtube.controller.ts
+++ src/youtube/youtube.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
+import { YoutubeService } from './youtube.service';
 
 @Controller('youtube')
-export class YoutubeController {}
+export class YoutubeController {
+  constructor(private readonly youtubeService: YoutubeService) { }
+
+  @Get('search')
+  async search(@Query('query') query: string, @Query('maxResults') maxResults: number = 10) {
+    if (!query) {
+      return { error: 'Query parameter is required' };
+    }
+
+    return this.youtubeService.searchVideos(query, maxResults);
+  }
+}
